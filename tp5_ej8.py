124f7a1b9d5139bf50f45526b27d0b66b6ba18f3
Florencia Deva <florencia.devalucero@gmail.com>
2021-06-02 11:45:11 -0300

    Corrección del cifrado de César, código ASCII

--- tp5_ej8.py
+++ tp5_ej8.py
@@ -13,9 +13,19 @@ def codificacion(posicion, texto):
     for letra in texto:
         entero_letra = ord(letra)
         entero_letra_modificada = entero_letra + posicion
+        
+        if (entero_letra_modificada > 122): #Códo ascii: 122= z
+            entero_letra_modificada = entero_letra_modificada - 26
+            
+        elif (entero_letra_modificada > 90) and (entero_letra_modificada < 97): #90= Z , 97= a
+            entero_letra_modificada = entero_letra_modificada - 26 # 90 - 26 = A  
+            
+        elif entero_letra_modificada > 57 and entero_letra_modificada < 65: # 57= 9, 65= A
+            entero_letra_modificada = entero_letra_modificada - 10
+            
         letra_modificada = chr(entero_letra_modificada)
         lista_codificar.append(letra_modificada)
-    texto = " ".join(lista_codificar)
+    texto = "".join(lista_codificar)
     return texto
         
 
@@ -25,9 +35,19 @@ def decodificacion(texto, posicion):
     for letra in texto:
         entero_letra = ord(letra)
         entero_letra_modificada = entero_letra - posicion
+        
+        if (entero_letra_modificada < 97) and (entero_letra_modificada > 90):
+            entero_letra_modificada = entero_letra_modificada + 26
+            
+        elif (entero_letra_modificada < 65) and (entero_letra_modificada > 57):
+            entero_letra_modificada = entero_letra_modificada + 26
+            
+        elif (entero_letra_modificada < 48): # 48 = 0
+            entero_letra_modificada = entero_letra_modificada + 10
+            
         letra_modificada = chr(entero_letra_modificada)
         lista_codificar.append(letra_modificada)
-    texto = " ".join(lista_codificar)
+    texto = "".join(lista_codificar)
     return texto
 
 def prueba():
---
+++ tp5_ej9.py
@@ -0,0 +1,16 @@
+################
+# Florencia Lucero D'Eva - @florenciadevalucero
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# implementar una funcion que retorne una lista con todos los factoriones menores a 1.499.999
+def factoriones():
+    pass
+
+def prueba():
+    """Toda la interacción con el usuario va acá"""
+    pass
+
+if __name__ == "__main__":
+    prueba()
